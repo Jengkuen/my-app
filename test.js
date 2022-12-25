11c5a37118d57b9a72a73f160711588e69b10cbb
jengkuen <chujengkuen@gmail.com>
2022-12-25 15:05:47 +0800

    WHHEEEEEEe

---
+++ test.js
@@ -0,0 +1,8 @@
+let Equipments = []
+let Equipments_f = []
+let obj
+// format multi select field inputs
+for (let i=0; i<Equipments.length;i++) {
+    Equipments_f.push(JSON.parse({"name": Equipments[i]}))
+}
+console.log(Equipments_f)
